86b608823d32b4e4628f9215dfa955210a2d01f7
YeonsuBaek <86752528+YeonsuBaek@users.noreply.github.com>
2022-07-19 13:31:43 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int floor(int x, int y) {
+    int i, j, apart[15][15] = {0, };
+    
+    for (i = 0; i < 15; i++) {
+        apart[0][i] = i;
+    }
+    
+    for (i = 1; i < 15; i++) {
+        for (j = 1; j < 15; j++) {
+            apart[i][j] = apart[i-1][j] + apart[i][j-1];
+        }
+    }
+    
+    return apart[x][y];
+}
+
+int main() {
+    int T, k, n, i, result;
+    
+    scanf("%d", &T);
+    
+    for (i = 0; i < T; i++) {
+        scanf("%d", &k);
+        scanf("%d", &n);
+        result = floor(k, n);
+        printf("%d\n", result);
+    }
+    
+    return 0;
+}
