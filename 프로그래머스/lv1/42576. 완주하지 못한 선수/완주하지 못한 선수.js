de219d33d3f72c729f8ba121d2eee18c3dd5c700
YeonsuBaek <soothbugger@gmail.com>
2022-12-23 16:37:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 103.52 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,7 +1,7 @@
 function solution(participant, completion) {
   participant.sort();
   completion.sort();
-  for (let i = 0; i < completion.length; i++) {
+    for (let i = 0; i < completion.length; i++) {
     if (completion[i] !== participant[i]) return participant[i];
   }
   return participant[participant.length - 1];
