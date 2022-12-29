37a98350609368c260dbb1a1f1d33df25d359f3f
YeonsuBaek <logthesummer@gmail.com>
2022-12-29 11:51:25 +0900

    [level 1] Title: [1차] 다트 게임, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.js
@@ -0,0 +1,35 @@
+function solution(dartResult) {
+    const squareList = {
+        S: 1, D: 2, T: 3,
+    }
+    let numbers = [];
+    let current = 0;
+    
+    [...dartResult].forEach((word, index) => {
+        if (!isNaN(word)) {
+            if (dartResult[index - 1] === "1") {
+                current = 10;
+            }
+            else current = +word;
+        }
+        if (word in squareList) {
+            numbers.push(Math.pow(current, squareList[word]));
+        }
+        
+        if (word === '*') {
+            numbers[numbers.length - 1] *= 2;
+            numbers[numbers.length - 2] *= 2;
+        }
+        if (word === '#') {
+            numbers[numbers.length - 1] *= -1;
+        }
+    })
+    
+    const answer = numbers.reduce((prev, cur) => {
+        return prev += cur;
+    }, 0);
+    
+    return answer;
+}
+
+[1, -2, 10]
