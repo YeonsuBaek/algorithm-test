46feaf00f9bf799cdcee8d1828843266a387adef
YeonsuBaek <logthesummer@gmail.com>
2023-04-25 06:53:33 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,13 @@
+def solution(cards1, cards2, goal):
+    index1 = 0
+    index2 = 0
+    
+    for card in goal:
+        if index1 < len(cards1) and cards1[index1] == card:
+            index1 += 1
+        elif index2 < len(cards2) and cards2[index2] == card:
+            index2 += 1
+        else:
+            return "No"
+    
+    return "Yes"
