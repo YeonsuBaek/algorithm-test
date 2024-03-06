3b2f66b84b9c847276e4f14bdbdadb32324640ad
Yeonsu Summer <baekyeonsu2000@gmail.com>
2024-03-06 10:46:45 +0900

    [level 1] Title: 폰켓몬, Time: 0.26 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,11 +1,5 @@
 function solution(nums) {
-    const SELECTION_NUM = nums.length / 2
-    const mons = []
-    nums.forEach((num) => {
-        if (!mons.includes(num)) {
-            mons.push(num)
-        }
-    })
-    const monsLength = mons.length
-    return monsLength <= SELECTION_NUM ? monsLength : SELECTION_NUM
+    const max = nums.length / 2
+    const mons = [...new Set(nums)]
+    return mons.length > max ? max : mons.length
 }
