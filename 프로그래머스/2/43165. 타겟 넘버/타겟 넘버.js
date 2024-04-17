b2ee6bfeef7ffe3d20b0b4576cc7a5d75cca01ab
Yeonsu Summer <baekyeonsu2000@gmail.com>
2024-04-17 11:25:36 +0900

    [level 2] Title: 타겟 넘버, Time: 1.16 ms, Memory: 35.5 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,16 +1,16 @@
 function solution(numbers, target) {
-    let count = 0
+    let result = 0
+    
     dfs(0, 0)
-
+    
     function dfs(idx, sum) {
         if (idx === numbers.length) {
-            if (sum === target) count++
+            if (sum === target) result += 1
             return
         }
-        
-        dfs(idx + 1, sum - numbers[idx])
         dfs(idx + 1, sum + numbers[idx])
+        dfs(idx + 1, sum - numbers[idx])
     }
     
-    return count
+    return result
 }
